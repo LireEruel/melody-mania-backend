a92e73449b0b10e63bbfcb6818e936da57d8846c
LireEruel <jsh001505@gmail.com>
2023-05-09 16:26:39 +0900

    회원 가입에 예외처리 추가

--- repository.py
+++ repository.py
@@ -12,7 +12,6 @@ class UserRepo():
             _user.append(user)
         return _user
 
-
     @staticmethod
     async def insert(user: User):
         id = str(uuid.uuid4())
@@ -23,3 +22,23 @@ class UserRepo():
             "password" : user.password
         }
         await database.get_collection('user').insert_one(_user)
+
+    @staticmethod
+    async def checkUniqueUser(user: User):
+        query = {
+            '$or': [
+                {'name': user.name},
+                {'email': user.email}
+            ]
+        }
+        res = {
+            'is_unique' : True,
+            'existing_user' : None
+        }
+
+        async for crt_user in database.get_collection('user').find(query):
+            res['is_unique'] = False
+            res['existing_user'] = crt_user
+            break
+        
+        return res
--- router.py
+++ router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from repository import UserRepo
 from model import User, Response
 
@@ -6,8 +6,19 @@ router = APIRouter()
 
 @router.post("/user/")
 async def signup(user: User):
-    _userList = await UserRepo.insert(user)
-    return Response(code=200, status="OK", message="Success").dict(exclude_none=True)
+    check_unique_res = await UserRepo.checkUniqueUser(user)
+    if  check_unique_res['is_unique'] :
+        print(check_unique_res['is_unique'])
+        await UserRepo.insert(user)
+        return Response(code=200, status="OK", message="Success").dict(exclude_none=True)
+    else :
+        same_user = check_unique_res['existing_user']
+        if user.name == same_user['name'] :
+            raise HTTPException(status_code=400,  detail="이미 같은 이름의 유저가 있습니다.")
+        elif user.email == same_user['email'] :
+            raise HTTPException(status_code=400,  detail="이미 등록된 이메일 입니다.")
+        else :
+             return Response(code=200, status="OK", message="Success").dict(exclude_none=True)
 
 @router.get("/user/")
 async def retrieve():
